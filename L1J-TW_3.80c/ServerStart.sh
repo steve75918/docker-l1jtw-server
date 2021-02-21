972ad91b05fbb9433ad68375e560a00e0d35183a
steve <steve75918@gmail.com>
2021-02-21 05:31:33 +0000

    Change server config.

--- L1J-TW_3.80c/ServerStart.sh
+++ L1J-TW_3.80c/ServerStart.sh
@@ -20,11 +20,11 @@ sed 's/mysql:\/\/localhost/mysql:\/\/'"$DB_HOST"'/g' server.properties > custom.
 sed 's/Password=password/Password='"$DB_PWD"'/g' server.properties > custom.server; cat custom.server > server.properties
 
 # Exp Rate
-sed 's/RateXp = 1.0/RateXp = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+sed 's/RateXp = 10.0/RateXp = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateLawful = 1.0/RateLawful = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateKarma = 1.0/RateKarma = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 sed 's/RateDropAdena = 1.0/RateDropAdena = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
-sed 's/RateDropItems = 1.0/RateDropItems = '"$RATE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
+sed 's/RateDropItems = 1.0/RateDropItems = '2'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
 
 # Enchant Chance
 sed 's/EnchantChanceWeapon = 0/EnchantChanceWeapon = '"$ENCHANT_CHANCE"'/g' rates.properties > custom.rates; cat custom.rates > rates.properties
